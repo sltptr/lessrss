b5567a61e412b23f2c213edb5cf12d86d856a108
sltptr <mike@sltptr.com>
2024-06-01 22:26:54 -0700

    NM

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 import enum
+import json
 import os
 from abc import ABC, abstractmethod
 from datetime import datetime
@@ -6,41 +7,37 @@ from typing import Optional
 
 import feedparser
 import joblib
+import numpy as np
 import pandas as pd
+import PyRSS2Gen
 import yaml
 from feedparser import FeedParserDict
-from flask import Flask
+from flask import Flask, send_file
 from flask_sqlalchemy import SQLAlchemy
 from openai import OpenAI
-from pydantic import BaseModel, RootModel
+from pydantic import BaseModel
 from sklearn.pipeline import Pipeline
 from sqlalchemy import DateTime, func
+from sqlalchemy.inspection import inspect
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 ### Config
 
 
-class ModelData(BaseModel):
-    name: str
+class ModelConfig(BaseModel):
     vote_weight: int
     active: bool
+    prompt: Optional[str] = None
 
 
-class ModelDataList(RootModel):
-    root: list[ModelData]
-
-
-class Feed(BaseModel):
+class FeedConfig(BaseModel):
     url: str
 
 
-class FeedList(RootModel):
-    root: list[Feed]
-
-
 class Config(BaseModel):
-    feeds: FeedList
-    models: ModelDataList
+    quorom: int
+    feeds: list[FeedConfig]
+    models: dict[str, ModelConfig]
 
 
 def load_config() -> Config:
@@ -62,7 +59,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////app/data/rss.db"
 db.init_app(app)
 
 
-class Sentiment(enum.Enum):
+class Label(enum.Enum):
     NEGATIVE = 0
     POSITIVE = 1
     NONE = 2
@@ -74,7 +71,7 @@ class Item(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     title: Mapped[str]
     link: Mapped[str]
-    sentiment: Mapped[Sentiment] = mapped_column(default=Sentiment.NONE)
+    label: Mapped[Label] = mapped_column(default=Label.NONE)
     description: Mapped[Optional[str]]
     author: Mapped[Optional[str]]
     category: Mapped[Optional[str]]
@@ -90,6 +87,12 @@ class Item(db.Model):
         DateTime, default=func.now(), onupdate=func.now(), nullable=False
     )
 
+    def serialize(self):
+        return {
+            c: getattr(self, c) if c != "label" else getattr(self, c).name
+            for c in inspect(self).attrs.keys()
+        }
+
 
 with app.app_context():
     db.create_all()
@@ -100,34 +103,35 @@ with app.app_context():
 
 class Model(ABC):
 
-    def __init__(self, data: ModelData) -> None:
+    def __init__(self, data: ModelConfig) -> None:
         self.vote_weight = data.vote_weight
         self.active = data.active
+        self.prompt = data.prompt
 
     @abstractmethod
     def load(self):
         pass
 
     @abstractmethod
-    def run(self, df: pd.DataFrame):
+    def run(self, df: pd.DataFrame) -> np.array:
         pass
 
 
 class TFIDFLogistic(Model):
 
-    def __init__(self, data: ModelData) -> None:
+    def __init__(self, data: ModelConfig) -> None:
         super().__init__(data)
 
     def load(self):
-        self.model: Pipeline = joblib.load("/app/data/tf-idf-logistic.joblib")
+        self.model: Pipeline = joblib.load("/app/data/models/tf-idf-logistic.joblib")
 
     def run(self, df):
-        return self.model.predict(df)
+        return self.model.predict(df["title"])
 
 
 class GPT(Model):
 
-    def __init__(self, data: ModelData) -> None:
+    def __init__(self, data: ModelConfig) -> None:
         super().__init__(data)
 
     def load(self):
@@ -135,63 +139,124 @@ class GPT(Model):
         self.client = OpenAI(api_key=OPENAI_API_KEY)
 
     def run(self, df):
-        pass
+        completion = self.client.chat.completions.create(
+            model="gpt-4o",
+            response_format={"type": "json_object"},
+            messages=[
+                {
+                    "role": "system",
+                    "content": "You are an assistant who labels RSS feed items. \
+            The user will first give a description of their preferences for RSS items. \
+            The user will then give a list of titles for feed items, i.e. ['Post A', 'Post B', 'Post C']. \
+            You will then respond with a JSON array of 0s and 1s where 0 means you don't think the user \
+            would like the RSS item based on the title, and a 1 means you do think the user would \
+            like the item, i.e. [0,1,1] means you think the user wouldn't like Post A but you think \
+            that the user would likes Post B and Post C. The JSON response should be in the format \
+            {labels: []}.",
+                },
+                {
+                    "role": "user",
+                    "content": f"The following is the description of my preferences for RSS items: {self.prompt}. \
+                    Here is the list of {df.shape[0]} titles that I need you to process: {df.title.tolist()}.",
+                },
+            ],
+        )
+        content_json = json.loads(completion.choices[0].message.content)
+        return np.array(content_json["labels"])
 
 
-### Filtering
-
-
-def download_feed(feed: Feed) -> FeedParserDict:
-    return feedparser.parse(feed.url)
-
-
-def download_all() -> list[FeedParserDict]:
+def load_tfidf():
     config = load_config()
-    return [download_feed(item) for item in config.feeds.root]
-
-
-def filter_feed(feed: Feed) -> Feed:
-    return feed
-
-
-def filter_all():
-    return
+    return TFIDFLogistic(config.models["tfidf"])
 
 
 ### Routes
 
 
-# Get raw feeds before filtering is applied
+def download_feed(url: str) -> FeedParserDict:
+    return feedparser.parse(url)
+
+
+def download_all() -> list[FeedParserDict]:
+    config = load_config()
+    return [download_feed(item.url) for item in config.feeds]
+
+
+def convert_to_dataframe(entries: list[FeedParserDict]):
+    data = [entry.get("title") for entry in entries]
+    return pd.DataFrame(data, columns=["title"])
+
+
+# Generate personalized feed
+@app.route("/generate", methods=["GET"])
+def generate():
+    config = load_config()
+    # (1) Load the models
+    tfidf = TFIDFLogistic(config.models["tfidf"])
+    gpt = GPT(config.models["gpt"])
+    models = [tfidf, gpt]
+    # (2) Get the raw feeds and entries
+    raw_feeds = download_all()
+    raw_entries = []
+    for feed in raw_feeds:
+        raw_entries.extend(feed.entries)
+    # (3) Use the raw entries to create the input dataframe
+    df = convert_to_dataframe(raw_entries)
+    df["votes"] = 0
+    # (4) Use models to apply predictions, count votes and filter entries
+    for model in models:
+        if not model.active:
+            continue
+        model.load()
+        preds = model.run(df)
+        df["votes"] += preds * model.vote_weight
+    filter = df.index[df["votes"] >= config.quorom]
+    filtered_entries = [raw_entries[i] for i in filter]
+    # (5) Construct new RSS file
+    items = []
+    for entry in filtered_entries:
+        item = PyRSS2Gen.RSSItem(
+            title=entry.get("title"),
+            link=entry.get("link"),
+            description=entry.get("description"),
+            author=entry.get("author"),
+            comments=entry.get("comments"),
+            enclosure=entry.get("enclosure"),
+            guid=entry.get("guid"),
+            pubDate=entry.get("pubDate"),
+            source=entry.get("source"),
+        )
+        items.append(item)
+
+    rss = PyRSS2Gen.RSS2(
+        title="RecoRSS",
+        link="http://news.ycombinator.com",
+        description="This is a combined feed from multiple sources.",
+        lastBuildDate=datetime.now(),
+        items=items,
+    )
+    with open("/app/data/feed.xml", "w", encoding="utf-8") as f:
+        rss.write_xml(f)
+
+    return df.to_json()
+
+
+# Get raw feed before filtering is applied
 @app.route("/raw", methods=["GET"])
 def raw():
-    return download_all()
-
-
-# Get filtered feeds
-@app.route("/filter", methods=["GET"])
-def filter():
-    items = data()
     return download_all()
 
 
 # Get database
 @app.route("/data", methods=["GET"])
 def data():
-    return [
-        {
-            "id": item.id,
-            "created_at": item.created_at,
-            "title": item.title,
-            "sentiment": item.sentiment.name,
-        }
-        for item in Item.query.all()
-    ]
+    return [item.serialize() for item in Item.query.all()]
 
 
-# Generate personalized feed
-@app.route("/generate", methods=["GET"])
-def generate():
-    return "todo"
+# Feed endpoint
+@app.route("/", methods=["GET"])
+def root():
+    return send_file("/app/data/feed.xml", mimetype="rss+xml")
 
 
 if __name__ == "__main__":
